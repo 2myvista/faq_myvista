3df9178d30a6394bcabcf47a1afc3776d2d27186
myvista <myvista@ya.ru>
2026-04-16 16:11:08 +0300

    fix: hide token

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,26 +3,37 @@ $projectRoot = Split-Path -Parent $scriptDir
 $envFile = Join-Path $projectRoot ".env"
 $cloudDir = Join-Path $projectRoot "cloud"
 
-if (!(Test-Path $envFile)) {
-  Write-Error ".env not found: $envFile"
-  exit 1
-}
-
+# загрузка env
 Get-Content $envFile | ForEach-Object {
-  if ($_ -match "=") {
-    $name, $value = $_ -split '=', 2
-    Set-Item -Path "env:$name" -Value $value
-  }
+	if ($_ -match "=") {
+		$name, $value = $_ -split '=', 2
+		Set-Item -Path "env:$name" -Value $value
+	}
 }
 
-yc serverless function version create `
-  --function-name notes-viewer `
-  --runtime nodejs22 `
-  --entrypoint index.handler `
-  --memory 128m `
-  --execution-timeout 3s `
-  --source-path $cloudDir `
-  --environment GITHUB_TOKEN=$env:GITHUB_TOKEN `
-  1> $null
+Write-Host "Deploying..." -ForegroundColor Yellow
 
-  Write-Host "Deploy done !!"
+try {
+	$result = yc serverless function version create `
+		--function-name notes-viewer `
+		--runtime nodejs22 `
+		--entrypoint index.handler `
+		--memory 128m `
+		--execution-timeout 3s `
+		--source-path $cloudDir `
+		--environment GITHUB_TOKEN=$env:GITHUB_TOKEN `
+		2>&1
+
+	if ($LASTEXITCODE -eq 0) {
+		Write-Host "Deploy OK )) " -ForegroundColor Green
+	}
+ else {
+		Write-Host "Deploy FAILED ((" -ForegroundColor Red
+		Write-Host $result
+	}
+
+}
+catch {
+	Write-Host "Deploy ERROR ((" -ForegroundColor Red
+	Write-Host $_
+}
